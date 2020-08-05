ad516287a4ae4c6e7342401c8fe3b4d95b045cf9
jing-project <2667937634@qq.com>
2020-08-05 15:22:39 +0800

    列表

--- Numpy_demo/demo_List03.py
+++ Numpy_demo/demo_List03.py
@@ -2,3 +2,14 @@
 # 作者：彭静
 # 开发时间: 2020-08-05 下午 12:23
 # 开发工具:PyCharm
+list1 = ['python',1997,'hello',2000,221,'2324','231']
+list2 = [1,2,3,4,5,6,7,8,9,10]
+print('list1[0]:',list1[0])
+
+print('list1[2:4]:',list1[2:4])
+print('list2[1:]:',list2[1:])
+print('list2[2:-1]:',list2[2:-1])
+print('list1[::2]:',list1[::2])
+print('list1[:]:',list1[:])
+
+print('list1[:]*2:',list1[:]*2)
